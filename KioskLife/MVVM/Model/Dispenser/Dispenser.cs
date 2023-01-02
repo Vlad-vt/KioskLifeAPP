95453c16cf93372dad2e5a72b7ba60a0f395ea5d
Влад <19Titov.vlad.Titov19@gmail.com>
2023-01-02 11:43:15 +0200

    changes

--- KioskLife/MVVM/Model/Dispenser/Dispenser.cs
+++ KioskLife/MVVM/Model/Dispenser/Dispenser.cs
@@ -1,9 +1,15 @@
-﻿using Newtonsoft.Json;
+﻿using KioskLife.Enums;
+using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.IO;
+using System.Net;
+using System.Text;
 
 namespace KioskLife.MVVM.Model.Dispenser
 {
-    public class Dispenser
+    public class Dispenser : Device
     {
         /// <summary>
         /// Dispenser COM name (COM1, COM2 , COM3 ...)
@@ -22,12 +28,13 @@ namespace KioskLife.MVVM.Model.Dispenser
 
         public string Errors { get; set; }
 
-        public Dispenser()
+        public Dispenser(string name, List<string> deviceErrors, string isOnline, DeviceType deviceType) : base(name, deviceErrors, isOnline, deviceType)
         {
 
         }
 
-        public Dispenser(string dispenserCOM, List<string> dispenserErrors, List<string> dispenserWarnings)
+        public Dispenser(string name, List<string> deviceErrors, string isOnline, DeviceType deviceType, string dispenserCOM, List<string> dispenserErrors, List<string> dispenserWarnings) : 
+            base(name, deviceErrors, isOnline, deviceType)
         {
             DispenserCOM = dispenserCOM;
             DispenserErrors = dispenserErrors;
@@ -35,6 +42,29 @@ namespace KioskLife.MVVM.Model.Dispenser
             for (int i = 0; i < DispenserErrors.Count; i++)
             {
                 Errors += DispenserErrors[i] + ",";
+            }
+        }
+
+        protected override void SendJSON()
+        {
+            try
+            {
+                using (WebClient webClient = new WebClient())
+                {
+                    NameValueCollection formData = new NameValueCollection();
+                    formData["Type"] = DeviceType.ToString();
+                    formData["Name"] = Name;
+                    formData["COM"] = DispenserCOM;
+                    formData["Errors"] = Errors;
+                    formData["Status"] = IsOnline;
+                    byte[] responseBytes = webClient.UploadValues("https://vr-kiosk.app/tntools/health_terminal.php", "POST", formData);
+                    string responsefromserver = Encoding.UTF8.GetString(responseBytes);
+                    webClient.Dispose();
+                }
+            }
+            catch (Exception e)
+            {
+                File.WriteAllText("log.txt", e.Message + "\n");
             }
         }
     }
