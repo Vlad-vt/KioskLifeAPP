fb5f6163d1d3556be90308d45bfd914bd6f927c2
Влад <19Titov.vlad.Titov19@gmail.com>
2023-08-21 17:15:38 +0300

    changes

--- KioskLife/obj/Release/net6.0-windows10.0.22000.0/App.g.i.cs
+++ KioskLife/obj/Release/net6.0-windows10.0.22000.0/App.g.i.cs
@@ -66,7 +66,7 @@ namespace KioskLife {
             
             #line default
             #line hidden
-            System.Uri resourceLocater = new System.Uri("/KioskLife;V1.0.0.0;component/app.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/KioskLife;component/app.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\App.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
